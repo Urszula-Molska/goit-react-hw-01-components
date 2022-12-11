2a034743d6ab8f857e2fd197e1996c2e47c9486f
Urszula Molska <urszula_molska@wp.pl>
2022-12-11 18:16:34 +0100

    update

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,18 +1,28 @@
 import PropTypes from 'prop-types';
-//import css from '../styles/Statistics.module.css';
+import css from '../styles/Statistics.module.css';
+
+const randomColor = () =>
+  `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+console.log(typeof randomBackground);
 
 export const Statistics = ({ title, stats }) => {
-  const StatsList = stats.map(statsItem => (
-    <li className="item" key={statsItem.id}>
-      <span className="label">{statsItem.label}</span>
-      <span className="percentage">{statsItem.percentage}</span>
-    </li>
-  ));
-
   return (
-    <section className="statistics">
-      {{ title }.length > 0 && <h2 className="title">{title}</h2>}
-      <ul className="stat-list">{StatsList}</ul>
+    <section className={css.statistics_section}>
+      <hr className={css.line}></hr>
+      {title.length > 0 && <h2 className={css.title}>{title}</h2>}
+      <ul className={css.statUl}>
+        {stats.map(statsItem => (
+          <li
+            style={{ backgroundColor: randomColor() }}
+            className={css.statLi}
+            key={statsItem.id}
+          >
+            <span className={css.label}>{statsItem.label}</span>
+            <span className={css.percentage}>{statsItem.percentage}%</span>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 };
